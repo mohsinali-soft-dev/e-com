bb4d23a31d5d89855bcb81c23394c933bb71586a
Mohsin Ali <155516378+mohsinali-soft-dev@users.noreply.github.com>
2026-06-25 20:07:10 +0500

    add product barcode model

---
+++ app/Models/ProductBarcode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductBarcode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'product_variant_id', 'barcode', 'type', 'is_primary'];
+
+    public function product(): BelongsTo { return $this->belongsTo(Product::class); }
+    public function variant(): BelongsTo { return $this->belongsTo(ProductVariant::class, 'product_variant_id'); }
+}
